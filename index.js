a3a30e7e7ed243ea0963166bfe251ba43113cef7
amuskatt <102235034+amuskatt@users.noreply.github.com>
2022-06-09 20:09:58 -0400

    commit tree 9917e39599ce2d215c4da01e3a3d5c17e3015325

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 5, 6, 7, 20);
+
